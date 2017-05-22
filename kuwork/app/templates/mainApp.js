d39226bc05d41b5cd0ae25576d742df2a8d8f4be
Nanerrr <jianmingzhao520@gmail.com>
2017-05-22 13:33:23 -0400

    added angular dependency

---
+++ kuwork/app/templates/mainApp.js
@@ -0,0 +1,14 @@
+//This file serves as the main controller for the whole application.
+var kuWork = angular.module('kuWorkApp', []);
+// kuWork.controller("kuWorkController", function($scope, $http){
+	
+// });
+
+(function() {
+kuWork.controller('kuWorkController', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http) {
+
+
+
+}]);
+
+})();
