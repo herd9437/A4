110d5a2363280bd6b995e9c73d3f0e679cfadfab
Nanerrr <jianmingzhao520@gmail.com>
2017-05-22 14:19:17 -0400

    added angular http services

--- kuwork/app/templates/mainApp.js
+++ kuwork/app/templates/mainApp.js
@@ -20,6 +20,42 @@ kuWork.controller('kuWorkController', ['$scope', '$rootScope', '$http', function
                 console.log("Error, could not retrieve activities");
             });
     };
+
+    this.retrieveStudents = function () {
+            console.log("retrieving student list");
+            $http({
+                method: 'GET',
+                url: '/app/students'               
+            }).then(function successCallback(response) {
+                console.log("success!");
+            }, function errorCallbac(response) {
+                console.log("Error, could not retrieve students");
+            });
+    };
+
+    this.retrieveCompanies = function () {
+            console.log("retrieving company list");
+            $http({
+                method: 'GET',
+                url: '/app/companies'               
+            }).then(function successCallback(response) {
+                console.log("success!");
+            }, function errorCallbac(response) {
+                console.log("Error, could not retrieve companies");
+            });
+    };
+
+    this.retrieveResidences = function () {
+            console.log("retrieving residence list");
+            $http({
+                method: 'GET',
+                url: '/app/residencies'               
+            }).then(function successCallback(response) {
+                console.log("success!");
+            }, function errorCallbac(response) {
+                console.log("Error, could not retrieve residencies");
+            });
+    };
 }]);
 
 })();
