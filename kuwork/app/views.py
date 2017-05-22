5795454bb909676787226ef144fd021aefd6e439
Austin Vecchio <au.vecchio@gmail.com>
2017-05-22 13:24:08 -0400

    Proper endpoints

--- kuwork/app/views.py
+++ kuwork/app/views.py
@@ -1,9 +1,16 @@
 import json
+from django.core.serializers.json import DjangoJSONEncoder
 
 from django.shortcuts import render
-
 from django.http import HttpResponse
 from django.template.response import TemplateResponse
+
+from app.models import Activity
+from app.models import Address
+from app.models import Company
+from app.models import Student
+from app.models import Residence
+from app.models import Vehicle
 
 # Create your views here.
 def index(request):
@@ -11,49 +18,17 @@ def index(request):
     return response
 
 def students(request):
-    if request.method == 'GET':
-        response_data = {}
-        response_data['result'] = 'error'
-        response_data['message'] = 'Some error message'
-        return HttpResponse(json.dumps(response_data), content_type="application/json")
-    elif request.method == 'POST':
-        response_data = {}
-        response_data['result'] = 'error'
-        response_data['message'] = 'Some error message'
-        return HttpResponse(json.dumps(response_data), content_type="application/json")
+        dictionaries = [ obj.as_dict() for obj in Student.objects.all() ]
+        return HttpResponse(json.dumps({"data": dictionaries}), content_type='application/json')
 
 def companies(request):
-    if request.method == 'GET':
-        response_data = {}
-        response_data['result'] = 'error'
-        response_data['message'] = 'Some error message'
-        return HttpResponse(json.dumps(response_data), content_type="application/json")
-    elif request.method == 'POST':
-        response_data = {}
-        response_data['result'] = 'error'
-        response_data['message'] = 'Some error message'
-        return HttpResponse(json.dumps(response_data), content_type="application/json")
+        dictionaries = [ obj.as_dict() for obj in Company.objects.all() ]
+        return HttpResponse(json.dumps({"data": dictionaries}), content_type='application/json')
 
 def residencies(request):
-    if request.method == 'GET':
-        response_data = {}
-        response_data['result'] = 'error'
-        response_data['message'] = 'Some error message'
-        return HttpResponse(json.dumps(response_data), content_type="application/json")
-    elif request.method == 'POST':
-        response_data = {}
-        response_data['result'] = 'error'
-        response_data['message'] = 'Some error message'
-        return HttpResponse(json.dumps(response_data), content_type="application/json")
+        dictionaries = [ obj.as_dict() for obj in Residence.objects.all() ]
+        return HttpResponse(json.dumps({"data": dictionaries}), content_type='application/json')
 
 def activities(request):
-    if request.method == 'GET':
-        response_data = {}
-        response_data['result'] = 'error'
-        response_data['message'] = 'Some error message'
-        return HttpResponse(json.dumps(response_data), content_type="application/json")
-    elif request.method == 'POST':
-        response_data = {}
-        response_data['result'] = 'error'
-        response_data['message'] = 'Some error message'
-        return HttpResponse(json.dumps(response_data), content_type="application/json")
+        dictionaries = [ obj.as_dict() for obj in Activity.objects.all() ]
+        return HttpResponse(json.dumps({"data": dictionaries}), content_type='application/json')
