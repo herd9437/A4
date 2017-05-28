8ee4f2b123695c6a18a1d5987c600739135a0f67
Austin Vecchio <au.vecchio@gmail.com>
2017-05-28 15:30:02 -0400

    Updated several more pages

--- rental/cust_index.php
+++ rental/cust_index.php
@@ -4,33 +4,38 @@ include_once("config.php");
 
 //fetching data in descending order (lastest entry first)
 //$result = mysql_query("SELECT * FROM users ORDER BY id DESC"); // mysql_query is deprecated
-$result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC"); // using mysqli_query instead
+$result = mysqli_query($mysqli, "SELECT * FROM customer"); // using mysqli_query instead
 ?>
 
 <html>
-<head>	
-	<title>Homepage</title>
+<head>
+	<title>Customer Homepage</title>
 </head>
 
 <body>
-<a href="add.html">Add New Data</a><br/><br/>
+<a href="add.html">Add New Customer</a><br/><br/>
 
 	<table width='80%' border=0>
 
 	<tr bgcolor='#CCCCCC'>
-		<td>Name</td>
-		<td>Age</td>
-		<td>Email</td>
-		<td>Update</td>
+		<td>First Name</td>
+		<td>Last Name</td>
+		<td>Street Address</td>
+		<td>City</td>
+		<td>Zip</td>
+		<td>Phone Number</td>
 	</tr>
-	<?php 
-	//while($res = mysql_fetch_array($result)) { // mysql_fetch_array is deprecated, we need to use mysqli_fetch_array 
-	while($res = mysqli_fetch_array($result)) { 		
+	<?php
+	//while($res = mysql_fetch_array($result)) { // mysql_fetch_array is deprecated, we need to use mysqli_fetch_array
+	while($res = mysqli_fetch_array($result)) {
 		echo "<tr>";
-		echo "<td>".$res['name']."</td>";
-		echo "<td>".$res['age']."</td>";
-		echo "<td>".$res['email']."</td>";	
-		echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";		
+		echo "<td>".$res['first_name']."</td>";
+		echo "<td>".$res['last_name']."</td>";
+		echo "<td>".$res['street_address']."</td>";
+		echo "<td>".$res['city']."</td>";
+		echo "<td>".$res['zip']."</td>";
+		echo "<td>".$res['phone_number']."</td>";
+		echo "<td><a href=\"cust_edit.php?id=$res[customer_id]\">Edit</a> | <a href=\"cust_delete.php?id=$res[customer_id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
 	}
 	?>
 	</table>
--- rental/rep_index.php
+++ rental/rep_index.php
@@ -4,33 +4,32 @@ include_once("config.php");
 
 //fetching data in descending order (lastest entry first)
 //$result = mysql_query("SELECT * FROM users ORDER BY id DESC"); // mysql_query is deprecated
-$result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC"); // using mysqli_query instead
+$result = mysqli_query($mysqli, "SELECT * FROM representative"); // using mysqli_query instead
 ?>
 
 <html>
-<head>	
-	<title>Homepage</title>
+<head>
+	<title>Representative Homepage</title>
 </head>
 
 <body>
-<a href="add.html">Add New Data</a><br/><br/>
+<a href="rep_add.html">Add New Representative</a><br/><br/>
 
 	<table width='80%' border=0>
 
 	<tr bgcolor='#CCCCCC'>
-		<td>Name</td>
-		<td>Age</td>
-		<td>Email</td>
-		<td>Update</td>
+		<td>First Name</td>
+		<td>Last Name</td>
+		<td>Phone Number</td>
 	</tr>
-	<?php 
-	//while($res = mysql_fetch_array($result)) { // mysql_fetch_array is deprecated, we need to use mysqli_fetch_array 
-	while($res = mysqli_fetch_array($result)) { 		
+	<?php
+	//while($res = mysql_fetch_array($result)) { // mysql_fetch_array is deprecated, we need to use mysqli_fetch_array
+	while($res = mysqli_fetch_array($result)) {
 		echo "<tr>";
-		echo "<td>".$res['name']."</td>";
-		echo "<td>".$res['age']."</td>";
-		echo "<td>".$res['email']."</td>";	
-		echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";		
+		echo "<td>".$res['first_name']."</td>";
+		echo "<td>".$res['last_name']."</td>";
+		echo "<td>".$res['phone_number']."</td>";
+		echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
 	}
 	?>
 	</table>
