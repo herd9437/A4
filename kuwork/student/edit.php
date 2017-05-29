8f91454ccf7ae8aa29d113d62e3032f51bba3ff3
John Herdman <johnherdman@Johns-MacBook-Pro-2.local>
2017-05-29 12:33:37 -0400

    modified edit file for student

--- kuwork/student/edit.php
+++ kuwork/student/edit.php
@@ -5,29 +5,37 @@ include_once("config.php");
 if(isset($_POST['update']))
 {	
 
-	$id = mysqli_real_escape_string($mysqli, $_POST['id']);
+	$email = mysqli_real_escape_string($mysqli, $_POST['email']);
 	
+	$phone_number = mysqli_real_escape_string($mysqli, $_POST['phone_number']);
 	$name = mysqli_real_escape_string($mysqli, $_POST['name']);
-	$age = mysqli_real_escape_string($mysqli, $_POST['age']);
-	$email = mysqli_real_escape_string($mysqli, $_POST['email']);	
+	$degree = mysqli_real_escape_string($mysqli, $_POST['degree']);	
+	$major = mysqli_real_escape_string($mysqli, $_POST['major']);
+	$class_standing = mysqli_real_escape_string($mysqli, $_POST['class_standing']);
+	$company_name = mysqli_real_escape_string($mysqli, $_POST['company_name']);
+	$residence_id = mysqli_real_escape_string($mysqli, $_POST['residence_id']);
 	
 	// checking empty fields
-	if(empty($name) || empty($age) || empty($email)) {	
-			
+	if(empty($email) || empty($name) || empty($degree) || empty($major)) {
+
+		if(empty($email)) {
+			echo "<font color='red'>Email field is empty.</font><br/>";
+		}
+
 		if(empty($name)) {
 			echo "<font color='red'>Name field is empty.</font><br/>";
 		}
-		
-		if(empty($age)) {
-			echo "<font color='red'>Age field is empty.</font><br/>";
+
+		if(empty($degree)) {
+			echo "<font color='red'>Degree field is empty.</font><br/>";
 		}
-		
-		if(empty($email)) {
-			echo "<font color='red'>Email field is empty.</font><br/>";
-		}		
+
+		if(empty($major)) {
+			echo "<font color='red'>Major field is empty.</font><br/>";
+		}
 	} else {	
 		//updating the table
-		$result = mysqli_query($mysqli, "UPDATE users SET name='$name',age='$age',email='$email' WHERE id=$id");
+		$result = mysqli_query($mysqli, "UPDATE student SET phone_number='$phone_number',name='$name',degree='$degree',major='$major',class_standing='$class_standing',company_name='$company_name',residence_id='$residence_id' WHERE email=$email");
 		
 		//redirectig to the display page. In our case, it is index.php
 		header("Location: index.php");
@@ -35,47 +43,20 @@ if(isset($_POST['update']))
 }
 ?>
 <?php
-//getting id from url
+//getting email (primary key) from url
 $email = $_GET['email'];
 
-//selecting data associated with this particular id
+//selecting data associated with this particular email
 $result = mysqli_query($mysqli, "SELECT * FROM student WHERE email=$email");
 
 while($res = mysqli_fetch_array($result))
 {
+	$phone_number = $res['phone_number'];
 	$name = $res['name'];
-	$age = $res['age'];
-	$email = $res['email'];
+	$degree = $res['degree'];
+	$major = $res['major'];
+	$class_standing = $res['class_standing'];
+	$company_name = $res['company_name'];
+	$residence_id = $res['residence_id'];
 }
 ?>
-<html>
-<head>	
-	<title>Edit Data</title>
-</head>
-
-<body>
-	<a href="index.php">Home</a>
-	<br/><br/>
-	
-	<form name="form1" method="post" action="edit.php">
-		<table border="0">
-			<tr> 
-				<td>Name</td>
-				<td><input type="text" name="name" value="<?php echo $name;?>"></td>
-			</tr>
-			<tr> 
-				<td>Age</td>
-				<td><input type="text" name="age" value="<?php echo $age;?>"></td>
-			</tr>
-			<tr> 
-				<td>Email</td>
-				<td><input type="text" name="email" value="<?php echo $email;?>"></td>
-			</tr>
-			<tr>
-				<td><input type="hidden" name="id" value=<?php echo $_GET['id'];?>></td>
-				<td><input type="submit" name="update" value="Update"></td>
-			</tr>
-		</table>
-	</form>
-</body>
-</html>
