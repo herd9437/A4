01fc9dabb7f5ca9439bf2af10cdea4162d1e4139
Austin Vecchio <au.vecchio@gmail.com>
2017-06-03 23:10:14 -0400

    Updated bills

--- rental/cust_bill.php
+++ rental/cust_bill.php
@@ -5,6 +5,14 @@ $rep_id = $_GET['rep_id'];
 //selecting data associated with this particular id
 $result = mysqli_query($mysqli, "SELECT * FROM representative WHERE rep_id=$rep_id");
 
+SELECT
+cu.first_name, cu.last_name, cu.street_address, cu.city, cu.state, cu.zip,
+cu.phone_number, co.name, co.street_address, co.city, co.state, co.zip, co.account_number,
+cu.credit_card_type, cu.credit_card_number, ca.license_number, ca.make, ca.model, r.mileage_plan,
+r.insurance_plan, r.rate_period, r.rate, r.date_rented, r.time_rented, r.date_returned, r.time_returned,
+r.start_miles, r.end_miles, r.gas_level, r.base_charge, r.mileage_charge, r.insurance_charge, r.gas_charge,
+r.tax, r.final_charge
+
 while($res = mysqli_fetch_array($result))
 {
 	$first_name = $res['first_name'];
@@ -43,3 +51,13 @@ while($res = mysqli_fetch_array($result))
 	</form>
 </body>
 </html>
+
+<?php
+$result = mysqli_query($mysqli, "SELECT * FROM representative WHERE rep_id=$rep_id");
+echo "<select>"
+while($res = mysqli_fetch_array($result))
+{
+	echo "<option></option>"
+}
+echo "</select>"
+?>
